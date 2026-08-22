1579401ab020518d830732094aff1eecc9aee444
Musa Adam Babatunde <musaadambabatunde@gmail.com>
2026-08-22 06:44:43 +0100

    Fix BASE_DIR in Settings

--- bacbok/bacbok/settings.py
+++ bacbok/bacbok/settings.py
@@ -16,7 +16,7 @@ import os
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parentBASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
